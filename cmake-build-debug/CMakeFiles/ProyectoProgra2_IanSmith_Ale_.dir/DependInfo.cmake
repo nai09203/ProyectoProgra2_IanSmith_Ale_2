9cb13f6e3c764367979ebc8bad9da032a0ade923
aabarcac04 <aabarcac04@gmail.com>
2026-05-12 23:06:24 -0600

    actualizado

---
+++ cmake-build-debug/CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/CalcularPrioridad.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/CalcularPrioridad.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/CalcularPrioridad.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/CalibrarDeco.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/CalibrarDeco.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/CalibrarDeco.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/ComputadoraLab.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/ComputadoraLab.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/ComputadoraLab.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/EjecutorMantenimiento.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/EjecutorMantenimiento.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/EjecutorMantenimiento.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/Equipo.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/Equipo.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/Equipo.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/FabricaMantenimiento.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/FabricaMantenimiento.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/FabricaMantenimiento.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/GestorArchivos.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/GestorArchivos.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/GestorArchivos.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/Incidencia.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/Incidencia.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/Incidencia.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/LimpiezaDeco.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/LimpiezaDeco.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/LimpiezaDeco.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/MantenimientoBase.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/MantenimientoBase.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/MantenimientoBase.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/MantenimientoDecorator.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/MantenimientoDecorator.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/MantenimientoDecorator.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/MicroscopioLab.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/MicroscopioLab.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/MicroscopioLab.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/OrdenadorEquipos.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/OrdenadorEquipos.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/OrdenadorEquipos.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/Planificador.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/Planificador.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/Planificador.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/Reportador.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/Reportador.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/Reportador.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/RevisionDeco.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/RevisionDeco.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/RevisionDeco.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/Simulador.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/Simulador.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/Simulador.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/TextEquipo.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/TextEquipo.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/TextEquipo.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/TextIncidencia.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/TextIncidencia.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/TextIncidencia.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/ValidadorArchivos.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/ValidadorArchivos.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/ValidadorArchivos.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/ValidadorEquipo.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/ValidadorEquipo.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/ValidadorEquipo.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/ValidadorIncidencia.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/ValidadorIncidencia.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/ValidadorIncidencia.cpp.obj.d"
+  "D:/ING SISTEMAS/PROGRA II/PROYECTO I/main.cpp" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/main.cpp.obj" "gcc" "CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ProyectoProgra2_IanSmith_Ale_.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ProyectoProgra2_IanSmith_Ale_.
+# This may be replaced when dependencies are built.
